3fca31d44ecc03669d2ae6df48d67c02c743e83a
retzl4ff <joaoaretzlaff@gmail.com>
2025-04-23 01:11:18 -0300

    `feat`: adding virtual environment to gitignore; import TensorFlow to use Keras; create functions to shuffle the data (preventing order bias) and to split it into training, validation, and test sets.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+from tensorflow import keras
+
 from scipy.io import loadmat
 
 def one_hot_encoding(labels_dimension):
@@ -13,48 +16,73 @@ def one_hot_encoding(labels_dimension):
 
     return one_hot_labels
 
-def normalize_image(image_matrix=[]):
-    return np.array(image_matrix) / 255
+def normalize_image(image_matrix=[]): #Normalize image matrix with values between 0 and 1
+    image = image_matrix.reshape(28, 28)
+    image = np.fliplr(np.fliplr(image.T))
+    return np.array(image) / 255
 
 class EMNISTHandler:
-    def __init__(self, data_path, mapping_label_path):
+    def __init__(self, data_path, mapping_label_path): #Initialize variables and call load functions
         self.data_path = data_path
         self.mapping_path = mapping_label_path
         self.label_mapping = {}
-        self.image_labels = []
-        self.normalized_image = []
+        self.images_matrix = []
+        self.images_labels = []
+        self.shuffled_images = []
+        self.shuffled_labels = []
         self._load_data()
         self._load_mapping()
 
-    def __str__(self):
-        return (
-            f"EMNISTHandler with {len(self.image_matrix)} images, "
-            f"{len(self.label_mapping)} labels loaded from:\n"
-            f"- Data: {self.data_path}\n"
-            f"- Mapping: {self.mapping_path}"
-        )
-
-    def _load_data(self):
+    def _load_data(self): #Load the dataset layer with images and labels
         data = loadmat(self.data_path)
-        image_matrix = data['dataset'][0][0][0][0][0][0]
-        self.image_labels = data['dataset'][0][0][0][0][0][1]
-        self.normalized_image = normalize_image(image_matrix)
+        images_matrix = data['dataset'][0][0][0][0][0][0]
+        images_labels = data['dataset'][0][0][0][0][0][1]
+        self.images_matrix = images_matrix
+        self.images_labels = images_labels
 
-    def _load_mapping(self):
+        self.shuffled_images, self.shuffled_labels = self._shuffle_data(images_matrix, images_labels)
+        (train_images, train_labels), (validation_images, validation_labels), (test_images, test_labels) = self._split_data(self.shuffled_images, self.shuffled_labels)
+
+
+    def _load_mapping(self): #Load the mapping file creating a dictionary
         with open(self.mapping_path, 'r') as f:
             for line in f:
                 label, ascii_code = line.strip().split()
                 self.label_mapping[int(label)] = chr(int(ascii_code))
 
-    def display_sample_images(self, num_samples=5):
+    def _shuffle_data(self, images_matrix, images_labels):
+        new_indexes = np.random.permutation(len(images_matrix))
+        shuffled_images = images_matrix[new_indexes]
+        shuffled_labels = images_labels[new_indexes]
+        return shuffled_images, shuffled_labels
+    
+    def _split_data(self, shuffled_images, shuffled_labels, train_size=0.8, validation_size=0.1):
+        total_size = len(shuffled_images)
+
+        #train and validation indexes
+        train_end = int(total_size * train_size)
+        validation_end = train_end + int(total_size * validation_size)
+
+        #slicing train, test and validation data
+        train_images = shuffled_images[:train_end]
+        train_labels = shuffled_labels[:train_end]
+
+        validation_images = shuffled_images[train_end:validation_end]
+        validation_labels = shuffled_labels[train_end:validation_end]
+
+        test_images = shuffled_images[validation_end:]
+        test_labels = shuffled_labels[validation_end:]
+
+        return (train_images, train_labels), (validation_images, validation_labels), (test_images, test_labels)
+
+
+    def display_sample_images(self, num_samples=5): #A method to display image samples
         for i in range(num_samples):
-            original_matrix = self.normalized_image[i].reshape(28, 28)
-            image = np.fliplr(np.fliplr(original_matrix.T))
-            label = self.image_labels[i][0]
-            plt.imshow(image, cmap='gray')
+            show_image = normalize_image(image_matrix=self.images_matrix[i])
+            label = self.images_labels[i][0]
+            plt.imshow(show_image, cmap='gray')
             plt.title(f'Label: {self.label_mapping[label]}')
             plt.axis('off')
             plt.show()
 
 emnist = EMNISTHandler(data_path='./matlab/emnist-balanced.mat',mapping_label_path='./matlab/emnist-balanced-mapping.txt')
-emnist.display_sample_images(5)
